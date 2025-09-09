15d51a858cbbb51f90c7dd3fad5219392f25cbbe
RomanMark <romanmarkilov@gmail.com>
2025-09-09 21:09:08 +0300

    added variables and some simple mockup

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,48 @@ class ProfileCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const String name = "Roman Markilov";
+    const String position = "Flutter Developer";
+    const String location = "Tallinn,Estonia";
+    const String email = "romanmarkilov@gmail.com";
+    const String phone = "+37256381344";
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile Card'),
-        backgroundColor: Colors.green,
+      body: Center(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            ClipRRect(
+                child: Column(
+                  children: [
+                    SizedBox(height: 250),
+                    Text(
+                      name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                      ),
+                    ),
+                    Text(position, style: TextStyle(fontSize: 20)),
+                    SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [Text(email), Text(location), Text(phone)],
+                    ),
+                  ],
+                ),
+            ),
+            const Positioned(
+              top: 100,
+              left: 150,
+              child: CircleAvatar(
+                backgroundImage: AssetImage(AutofillHints.addressCity),
+                radius: 60,
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Center(child: Text('About Me')),
     );
   }
 }
