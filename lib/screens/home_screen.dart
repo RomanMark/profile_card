ec8596e902225759b257b475feff04211ebb1d31
RomanMark <romanmarkilov@gmail.com>
2025-09-12 19:39:34 +0300

    added profile picture and started developing the home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../appearance/app_colors.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -10,9 +12,39 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/profile.jpg"),
+                    radius: 85,
+                  ),
+                  SizedBox(height: 18),
+                  Text(
+                    "Roman Markilov",
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  SizedBox(
+                    height: 14,
+                    width: 100,
+                    child: Divider(thickness: 2, color: AppColors.secondary),
+                  ),
+                  Text(
+                    "App Developer",
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
